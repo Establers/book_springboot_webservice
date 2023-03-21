77271948be439c31c525723f27194ccc629fd583
establers <establers@naver.com>
2023-03-21 23:13:39 +0900

    누락

---
+++ src/main/java/com/hwan/book/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.hwan.book.springboot;
+import com.sun.swing.internal.plaf.metal.resources.metal;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
